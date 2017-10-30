e81026e167c184dc7fe7e25b7ce4f2bc61d5abd6
Risen Escano <escanorisenrey@yahoo.com>
2017-10-30 14:03:00 +0800

    NBALIST

---
+++ my-app.js
@@ -0,0 +1,32 @@
+retrieve();
+document.addEvenetListener('click',function(){
+	let input = document.getElementById('field_input').value;
+	document.getElementById('field_input').value = '';
+	if(input != ""){
+	    let count = (localStorage.getItem('count') == null)?0:localStorage.getItem('count');
+	    localStorage.setItem('count',(count*1)=1);
+	    console.log(count);
+	    localStorage.setItem(`input-${(count)}`,input);
+
+	    retrieve();
+	}
+}};
+
+function retrieve(){
+	 let data = [];
+	 let count = Number(localStorage.getItem('count'));
+
+	 for(let x = 0; x<count; x++){
+	     data.push(localStorage.getItem(`input-${x}`));
+	 }
+
+     list(data);
+}
+
+ function list(data){
+     let content = "";
+     for(let value of data){
+         content += `<li>{value}</li>`;
+     }
+     document.getElementById('display_list').innerHTML = `<ol>${content}</ol>`;
+ }
